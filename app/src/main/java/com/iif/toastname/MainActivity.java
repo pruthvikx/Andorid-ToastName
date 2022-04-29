bc4734a76938216e17d32e501550f9ac692bdc41
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-04-29 16:46:34 +0530

    simple android app with plain edit text to enter your name and press button to see the toast message with greetings "Hello"

---
+++ app/src/main/java/com/iif/toastname/MainActivity.java
@@ -0,0 +1,24 @@
+package com.iif.toastname;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickButtonFunction(View view){
+
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Toast.makeText(this, "Hello " + nameEditText.getText().toString() , Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
